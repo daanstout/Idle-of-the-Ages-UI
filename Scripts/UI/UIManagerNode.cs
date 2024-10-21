2d237f8ad87d97e515f82412ad0a1a34e92781f8
Daan Stout <daanstout@proton.me>
2024-10-21 16:30:47 +0200

    ui work

---
+++ Scripts/UI/UIManagerNode.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdleOfTheAges.Scripts.UI;
+
+public partial class UIManagerNode : Node {
+    [Export] private Panel headerPanel;
+    [Export] private Panel footerPanel;
+    [Export] private Panel tabListPanel;
+    [Export] private Panel mainUIPanel;
+}
